5b7402068eaf312c705f71f1e4c559f10b24cc4b
Agraj Yadav <agrajy10@gmail.com>
2025-01-08 22:15:43 +0530

    setup tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { baselightTheme } from "./utils/theme/DefaultColors.tsx";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={baselightTheme}>
       <CssBaseline />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 );
